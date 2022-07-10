95fa1dd996a3eb6980e3d4b59130116a8ef958b5
Virtuouz <giovanni_plascencia@hotmail.com>
2022-07-10 10:32:52 -0600

    Implement CTA

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,11 @@
 module.exports=config=>{
     config.addPassthroughCopy('./src/images/')
+
+    config.addCollection('work', collection => {
+        return collection
+          .getFilteredByGlob('./src/work/*.md')
+          .sort((a, b) => (Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1));
+      });
     return{
         markdownTemplateEngine:'njk',
         dataTemplateEngine:'njk',
